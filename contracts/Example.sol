f6686e09e1435c13a964bb718d8569eb1db2e114
Arjit Khullar <arjitkhullar@gmail.com>
2018-08-25 18:08:59 -0400

    added support for mappings, events

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.4.9;
+
+
 contract Example {
+
     uint128 public num = 5;
+    event NumChanged (uint128);
+
     function add(uint128 a) public returns (uint128) {
-    return uint128(num+a);
+        return num+a;
     }
+
     function setA(uint128 a) public {
         num = a;
+        NumChanged(num);
     }
 }
